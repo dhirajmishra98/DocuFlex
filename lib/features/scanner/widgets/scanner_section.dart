9bf1e4449e7f122691e7bb9d3a06805214c88897
dhirajmishra98 <gobindmishra23@gmail.com>
2024-11-19 23:14:43 +0530

    remove unused import

--- lib/features/scanner/widgets/scanner_section.dart
+++ lib/features/scanner/widgets/scanner_section.dart
@@ -8,8 +8,6 @@ import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:pdf/pdf.dart';
-import 'package:pdf/widgets.dart' as pw;
 import 'package:permission_handler/permission_handler.dart';
 
 import '../../../utils/pdf_utils.dart';
